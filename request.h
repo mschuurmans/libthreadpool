f98c3336b91a33d9e75261cdef86549a4c900ebe
Michiel Schuurmans <michielschuurmans@gmail.com>
2020-12-14 22:53:23 +0100

    Request: Add documentation
    Signed-off-by: Michiel Schuurmans <michielschuurmans@gmail.com>

--- request.h
+++ request.h
@@ -8,9 +8,29 @@ enum REQUEST_STATE {
 };
 
 struct REQUEST {
+	/*
+	 * Identifier of the request. This will be set when
+	 * the request is enqueued and is the request number 
+	 * count for the thread pool at that given moment.
+	 */
 	int			id;
+
+	/*
+	 * Priority is used to determen on which fifo the 
+	 * request is placed. The 0 will be checked first
+	 * and then till NUM_FIFOS
+	 */
 	int			priority;
+
+	/*
+	 * State which the current request is in.
+	 */
 	enum REQUEST_STATE	state;
+
+	/*
+	 * Callback method to handle the request. This method
+	 * will be called from the worker thread.
+	 */
 	int (*process)(struct REQUEST *request);
 };
 
